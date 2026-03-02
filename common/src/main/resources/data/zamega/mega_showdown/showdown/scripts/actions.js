5c2ff612850a646be230df3f9d8657db9ec2d30f
ZycorZayden <zycor.zayden@gmail.com>
2026-03-02 00:50:46 -0600

    Frosslasite and New Dragonite model
    - Removed some innecesary things in scripts
    - New Mega Dragonite Model
    - Frosslassite Fix

--- common/src/main/resources/data/zamega/mega_showdown/showdown/scripts/actions.js
+++ common/src/main/resources/data/zamega/mega_showdown/showdown/scripts/actions.js
@@ -25,96 +25,44 @@
         }
       }
     }
-
-    switch (speciesid) {
-      case "Raichu-Mega-X": {
-        if (pokemon.m._raichuXPatched) break;
-        pokemon.m._raichuXPatched = true;
-
-        const baseSpecies = this.battle.dex.species.get("Raichu-Mega-X");
-        const patched = this.battle.dex.deepClone(baseSpecies);
-
-        patched.baseStats = {
-          hp: 60,
-          atk: 135,
-          def: 95,
-          spa: 90,
-          spd: 95,
-          spe: 110,
-        };
-        patched.abilities = { 0: "Levitate", H: "Levitate" };
-        patched.types = ["Electric"];
-        patched.heightm = 1.2;
-        patched.weightkg = 38.0;
-
-        pokemon.formeChange(patched, pokemon.getItem(), true);
-        break;
-      }
-
-      case "Raichu-Mega-Y": {
-        if (pokemon.m._raichuYPatched) break;
-        pokemon.m._raichuYPatched = true;
-
-        const baseSpecies = this.battle.dex.species.get("Raichu-Mega-Y");
-        const patched = this.battle.dex.deepClone(baseSpecies);
-
-        patched.baseStats = {
-          hp: 60,
-          atk: 100,
-          def: 55,
-          spa: 160,
-          spd: 80,
-          spe: 130,
-        };
-        patched.abilities = { 0: "Transistor", H: "Transistor"};
-        patched.types = ["Electric"];
-        patched.heightm = 1.0;
-        patched.weightkg = 26.0;
-
-        pokemon.formeChange(patched, pokemon.getItem(), true);
-        break;
-      }
-
-      default:
-        pokemon.formeChange(speciesid, pokemon.getItem(), true);
-        break;
-    }
-
-
+    pokemon.formeChange(speciesid, pokemon.getItem(), true);
     const wasMega = pokemon.canMegaEvo;
     for (const ally of pokemon.side.pokemon) {
       if (wasMega) ally.canMegaEvo = null;
       else ally.canUltraBurst = null;
     }
-
     this.battle.runEvent("AfterMega", pokemon);
     return true;
   },
+
   canMegaEvo(pokemon) {
     const species = pokemon.species;
-    if (pokemon.ability === 'aurabreak'&& pokemon.terastallized) return null;
-
+    if (pokemon.ability === 'aurabreak' && pokemon.terastallized) return null;
     const item = pokemon.getItem();
-
+    if (item.id === "meowsticite") {
+      if (species.name === "Meowstic-F") {
+        return "Meowstic-F-Mega";
+      }
+      if (species.name === "Meowstic") {
+        return "Meowstic-Mega";
+      }
+    }
     if (
       species.baseSpecies === "Rayquaza" &&
       (pokemon.terastallized || item.zMove)
     ) {
       return null;
     }
-
     if (
       species.baseSpecies === "Rayquaza" &&
       pokemon.baseMoves.includes("dragonascent")
     ) {
       return "Rayquaza-Mega";
     }
-
     const megaKey = species.otherFormes?.find(form =>
       /.*-Mega(-[a-zA-Z])?/.test(form)
     );
     const megaForme = megaKey && this.dex.species.get(megaKey);
-
     if (
       (this.battle.gen <= 7 || this.battle.ruleTable.has("+pokemontag:past")) &&
       megaForme?.requiredMove &&
@@ -123,14 +71,12 @@
     ) {
       return megaForme.name;
     }
-
     if (
       item.megaEvolves?.includes(species.name) &&
       item.megaStone !== species.name
     ) {
       return item.megaStone;
     }
-
     return null;
   }
 }
